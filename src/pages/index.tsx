20a5685df67ef19e671e5cd2957eac447a5e6eee
Michael Soto <67668995+nicksoto1@users.noreply.github.com>
2021-06-22 22:21:48 -0300

    aula3-incompleto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Head from "next/head";
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
 import styles from "../styles/pages/Home.module.css";
-import Head from "next/head";
 
 export default function Home() {
   return (
@@ -11,6 +14,7 @@ export default function Home() {
       <Head>
         <title>Inicio | move.it</title>
       </Head>
+      
       <ExperienceBar />
 
       <section>
@@ -19,7 +23,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
