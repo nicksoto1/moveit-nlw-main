13e7608e3ce6eef65c98efd7a3c0c11542e21145
michael <michael_nickolas@hotmail.com>
2021-07-02 18:36:23 -0300

    test screen

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,11 +1,23 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
+import {CountdownContext} from '../contexts/CountdownContext'
 
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-  const { activeChallenge,resetChallenge } = useContext(ChallengesContext);
- 
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } =useContext(CountdownContext);
+
+function handleChallengeSucceeded() {
+  completeChallenge()
+  resetCountdown()
+}
+
+function handleChallengeFailed() {
+  resetChallenge()
+  resetCountdown()
+}
+
   return (
     <div className={styles.challengeBoxContainer}>
       {activeChallenge ? (
@@ -13,17 +25,27 @@ export function ChallengeBox() {
           <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src={`icons/${activeChallenge.type}.svg `} />
+          {activeChallenge.type === 'body' 
+              ? <img src="/icons/body.svg" /> 
+              : <img src="/icons/eye.svg" />
+            }
             <strong>Novo desafio</strong>
             <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
-            <button type="button" className={styles.challengeFailedButton}>
-							onClick={resetChallenge}
+            <button
+             type="button"
+              className={styles.challengeFailedButton}
+							onClick={handleChallengeFailed}
+              >
               Falhei
             </button>
-            <button type="button" className={styles.challengeSucceededButton}>
+            <button
+             type="button"
+              className={styles.challengeSucceededButton}
+              onClick={handleChallengeSucceeded}
+              >
               Completei
             </button>
           </footer>
@@ -42,4 +64,3 @@ export function ChallengeBox() {
 }
 
 
-// 00:57
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,90 @@
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState,
+  useContext,
+} from "react";
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CountdownContextData {
+  isActive: boolean;
+  hasFinished: boolean;
+  minutes: number;
+  seconds: number;
+  resetCountdown: () => void;
+  startCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  
+  function startCountdown() {
+    setIsActive(true);
+  }
+  
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(0.1 * 60);
+  }
+  
+
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      if (isActive) {
+        return "Você perderá o progresso do countdown até aqui, tem certeza?";
+      }
+    };
+  }, [isActive]);
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      startNewChallenge();
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [isActive, time]);
+
+
+
+  return (
+    <CountdownContext.Provider
+    value={{
+      isActive,
+      minutes,
+      seconds,
+      hasFinished,
+      resetCountdown,
+      startCountdown,
+    }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+}
+
+function useChallenges(): { startNewChallenge: any } {
+  throw new Error("Function not implemented.");
+}
+
